ff0dd04be6fffd8d521df845192720b0f2628696
Damien <90845231+Damien-Dupont@users.noreply.github.com>
2023-01-26 09:06:07 +0100

    UPDATED controller

--- backend/src/controllers/ContractControllers.js
+++ backend/src/controllers/ContractControllers.js
@@ -1,91 +1,89 @@
-const models = require("../old models");
+const models = require("../models");
+// const { jwtSign } = require("../services/jwt");
 
-const browse = (req, res) => {
-  models.contract
-    .findAll()
-    .then(([rows]) => {
-      res.send(rows);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+class ContractController {
+  static browse = (req, res) => {
+    models.contract
+      .findAll()
+      .then(([rows]) => {
+        res.send(rows);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
 
-const read = (req, res) => {
-  models.contract
-    .find(req.params.id)
-    .then(([rows]) => {
-      if (rows[0] == null) {
-        res.sendStatus(404);
-      } else {
-        res.send(rows[0]);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+  static read = (req, res) => {
+    models.contract
+      .find(req.params.id)
+      .then(([rows]) => {
+        if (rows[0] == null) {
+          res.sendStatus(404);
+        } else {
+          res.send(rows[0]);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
 
-const edit = (req, res) => {
-  const contract = req.body;
+  static edit = (req, res) => {
+    const contract = req.body;
 
-  // TODO validations (length, format...)
+    // TODO validations (length, format...)
 
-  contract.id = parseInt(req.params.id, 10);
+    contract.id = parseInt(req.params.id, 10);
 
-  models.contract
-    .update(contract)
-    .then(([result]) => {
-      if (result.affectedRows === 0) {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(204);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+    models.contract
+      .update(contract)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
 
-const add = (req, res) => {
-  const contract = req.body;
+  static add = (req, res) => {
+    const contract = req.body;
 
-  // TODO validations (length, format...)
+    // TODO validations (length, format...)
 
-  models.contract
-    .insert(contract)
-    .then(([result]) => {
-      res.location(`/contracts/${result.insertId}`).sendStatus(201);
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+    models.contract
+      .insert(contract)
+      .then(([result]) => {
+        res.location(`/contracts/${result.insertId}`).sendStatus(201);
+        // res.status(201).send({ ...req.body, id: result.insertId });
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
 
-const destroy = (req, res) => {
-  models.contract
-    .delete(req.params.id)
-    .then(([result]) => {
-      if (result.affectedRows === 0) {
-        res.sendStatus(404);
-      } else {
-        res.sendStatus(204);
-      }
-    })
-    .catch((err) => {
-      console.error(err);
-      res.sendStatus(500);
-    });
-};
+  static delete = (req, res) => {
+    models.contract
+      .delete(req.params.id)
+      .then(([result]) => {
+        if (result.affectedRows === 0) {
+          res.sendStatus(404);
+        } else {
+          res.sendStatus(204);
+        }
+      })
+      .catch((err) => {
+        console.error(err);
+        res.sendStatus(500);
+      });
+  };
+}
 
-module.exports = {
-  browse,
-  read,
-  edit,
-  add,
-  destroy,
-};
+module.exports = ContractController;
